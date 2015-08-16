bd12eee975096a89c1f5c1e49397747e39848623
Edvaldo Szymonek <edvaldoszy@gmail.com>
2015-08-16 16:18:19 -0300

    Adicionado atributo "code" para classe Output

--- web/module/Administrator/Helper/Output.php
+++ web/module/Administrator/Helper/Output.php
@@ -7,6 +7,21 @@ use Szy\Mvc\Helper\AbstractHelper;
 class Output extends AbstractHelper
 {
     /**
+     * OK code
+     */
+    const CODE_OK = 1;
+
+    /**
+     * Error code
+     */
+    const CODE_ERR = 0;
+
+    /**
+     * @var int
+     */
+    public $code = 0;
+
+    /**
      * @var string
      */
     public $status;
