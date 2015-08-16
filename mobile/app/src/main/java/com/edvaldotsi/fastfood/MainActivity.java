93894cd9a05ff739f848735b53d6743283d0cd8b
Edvaldo Szymonek <edvaldoszy@gmail.com>
2015-08-16 16:16:44 -0300

    Testando requisição http pelo Android

--- mobile/app/src/main/java/com/edvaldotsi/fastfood/MainActivity.java
+++ mobile/app/src/main/java/com/edvaldotsi/fastfood/MainActivity.java
@@ -1,9 +1,18 @@
 package com.edvaldotsi.fastfood;
 
+import android.content.ContentValues;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +42,37 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendLoginRequest(View v) {
+
+        try {
+            final URL url = new URL("http://192.168.0.16/login/index/json");
+
+            new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                        con.setDoOutput(true);
+                        con.setRequestMethod("POST");
+
+                        ContentValues cv = new ContentValues();
+                        cv.put("login", "edvaldo");
+                        cv.put("senha", "123");
+
+                        DataOutputStream data = new DataOutputStream(con.getOutputStream());
+                        data.writeBytes(cv.toString().replace(' ', '&'));
+
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                        System.out.println(reader.readLine());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
