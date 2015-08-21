adf31920b007455cc4bb9df017e6c38ddf05372e
Edvaldo Szymonek <edvaldoszy@gmail.com>
2015-08-21 19:00:26 -0300

    Exemplo de utilização da classe ServerRequest

--- mobile/app/src/main/java/com/edvaldotsi/fastfood/MainActivity.java
+++ mobile/app/src/main/java/com/edvaldotsi/fastfood/MainActivity.java
@@ -1,20 +1,15 @@
 package com.edvaldotsi.fastfood;
 
-import android.content.ContentValues;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import com.edvaldotsi.fastfood.request.RequestInterface;
+import com.edvaldotsi.fastfood.request.ServerRequest;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RequestInterface {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,35 +39,14 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void afterRequest(String json) {
+        System.out.println(json);
+    }
+
     public void sendLoginRequest(View v) {
 
-        try {
-            final URL url = new URL("http://192.168.0.16/login/index/json");
-
-            new Thread() {
-                @Override
-                public void run() {
-                    try {
-                        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                        con.setDoOutput(true);
-                        con.setRequestMethod("POST");
-
-                        ContentValues cv = new ContentValues();
-                        cv.put("login", "edvaldo");
-                        cv.put("senha", "123");
-
-                        DataOutputStream data = new DataOutputStream(con.getOutputStream());
-                        data.writeBytes(cv.toString().replace(' ', '&'));
-
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                        System.out.println(reader.readLine());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        ServerRequest request = new ServerRequest(this, this);
+        request.execute("http://192.168.115.99/login/");
     }
 }
