52182d29694b15ddd0c25410b9451f8139ae6caa
Edvaldo Szymonek <edvaldoszy@gmail.com>
2015-08-25 22:07:05 -0300

    Ajustando estrutura principal

--- web/public_html/index.php
+++ web/public_html/index.php
@@ -1,17 +1,37 @@
 <?php
 
-ini_set('display_errors', 'on');
+/**
+ * PHP does not show errors
+ */
+ini_set("display_errors", "on");
 
-require_once '../vendor/autoload.php';
+/**
+ * Require Composer autoload file
+ */
+require_once "../vendor/autoload.php";
 
 use Szy\Mvc\Application;
-use Szy\File\FileSystem;
+use Szy\Mvc\Route;
 
 define('BASE_PATH', dirname(__DIR__));
 define('PUBLIC_PATH', BASE_PATH . '/public_html');
 
-FileSystem::chdir(BASE_PATH);
+/**
+ * Change current directory
+ */
+chdir(BASE_PATH);
 
-$routes = require_once(BASE_PATH . '/config/routes.config.php');
-$config = require_once(BASE_PATH . '/config/application.config.php');
-Application::init($routes, $config);
+/**
+ * Init application configurations
+ */
+$app = Application::init(require_once(BASE_PATH . "/config/application.config.php"));
+
+/**
+ * Application routes
+ */
+$route = new Route(require_once(BASE_PATH . "/config/routes.config.php"));
+
+/**
+ * Run application
+ */
+$app->run($route);
